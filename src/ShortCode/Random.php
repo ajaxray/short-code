2abb6871fc4c764e6f9054b3a9b37149b435e944
sohag <shcseilacs@gmail.com>
2021-11-13 19:24:11 +0600

    Code formatted.

--- src/ShortCode/Random.php
+++ src/ShortCode/Random.php
@@ -13,41 +13,44 @@ namespace ShortCode;
 use ShortCode\Exception\UnexpectedCodeLength;
 
 /**
- * Random
- *
- * Generate Random code string of desired length and format
- *
- * @author Anis Uddin Ahmad <anis.programmer@gmail.com>
- */
-class Random extends Code {
-	/**
-	 * Get a random code of fixed length.
-	 *
-	 * @param int $length length of code, default 8
-	 * @param string $outputFormat One of Code::FORMAT_* constants. Default Code::FORMAT_ALNUM
-	 *
-	 * @return string
-	 */
-	public static function get($length = 8, $outputFormat = Code::FORMAT_ALNUM) {
-		static::throwUnlessAcceptable($outputFormat, $length);
+	* Random
+	*
+	* Generate Random code string of desired length and format
+	*
+	* @author Anis Uddin Ahmad <anis.programmer@gmail.com>
+	*/
+class Random extends Code
+{
+				/**
+					* Get a random code of fixed length.
+					*
+					* @param int $length length of code, default 8
+					* @param string $outputFormat One of Code::FORMAT_* constants. Default Code::FORMAT_ALNUM
+					*
+					* @return string
+					*/
+				public static function get($length = 8, $outputFormat = Code::FORMAT_ALNUM)
+				{
+								static::throwUnlessAcceptable($outputFormat, $length);
 
-		$number = rand(100, 900) . str_replace('.', '', microtime(true));
-		$output = self::convertBase($number, self::FORMAT_NUMBER, $outputFormat);
+								$number = rand(100, 900) . str_replace('.', '', microtime(true));
+								$output = self::convertBase($number, self::FORMAT_NUMBER, $outputFormat);
 
-		if(strlen($output) < $length) {
-			$output .= substr(str_shuffle($outputFormat . $outputFormat), 0, ($length - strlen($output)));
-		} else {
-			$output = substr($output, 0, $length);
-		}
+								if(strlen($output) < $length) {
+												$output .= substr(str_shuffle($outputFormat . $outputFormat), 0, ($length - strlen($output)));
+								} else {
+												$output = substr($output, 0, $length);
+								}
 
-		return $output;
-	}
+								return $output;
+				}
 
-	private static function throwUnlessAcceptable($type, $length) {
-		if($length > 20) {
-			$typeName = self::getTypeName($type);
-			throw new UnexpectedCodeLength("Code length $length is not acceptable for $typeName");
-		}
-	}
+				private static function throwUnlessAcceptable($type, $length)
+				{
+								if($length > 20) {
+												$typeName = self::getTypeName($type);
+												throw new UnexpectedCodeLength("Code length $length is not acceptable for $typeName");
+								}
+				}
 
 }
